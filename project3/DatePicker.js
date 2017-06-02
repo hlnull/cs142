2c579a19e7150d8b384db3b5cabad71db77f40fd
hlnull <manfeyn@qq.com>
2017-06-02 08:38:07 +0800

    my datepicker.js

---
+++ project3/DatePicker.js
@@ -0,0 +1,18 @@
+'use strick';
+
+class DatePicker {
+    constructor(id, callback) {
+        this.id = id;
+        this.callback = callback;
+    }
+
+    render(date) {
+        // 1. create a div and add it to the document.
+        
+        // 2. add a table in the div.
+
+        // 3. set the contents of the table.
+
+    }
+};
+
