31b81d2d4b1bd1d9551e863ff40971142db5c89a
hlnull <manfeyn@qq.com>
2017-06-06 01:09:14 +0800

    finish project3

--- project3/TableTemplate.js
+++ project3/TableTemplate.js
@@ -2,17 +2,31 @@
 
 class TableTemplate {
     static fillIn(id, dict, columnName) {
-        var table = document.getElementById(id);
-        var rows = table.rows;
-        var header = rows.item(0);
-        var proc = new Cs142TemplateProcessor(header.innerHTML);
-        var newHeader = proc.fillIn(dict);
+        let table = document.getElementById(id);
+        let rows = table.rows;
+        let header = rows.item(0);
+        let proc = new Cs142TemplateProcessor(header.innerHTML);
+        let newHeader = proc.fillIn(dict);
         header.innerHTML = newHeader;
 
+        let cols = [];
         if (columnName === undefined) {
-            // handle the table.
+            cols = Array.from(Array(header.cells.length).keys())
         } else {
-            // handle the column.
+            for (let i = 0; i < header.cells.length; ++ i) {
+                if (header.cells[i].innerHTML === columnName) {
+                    cols = [i];
+                }
+            }
+        }
+
+        for (let i = 1; i < table.rows.length; ++ i) {
+            let row = table.rows[i];
+            for (let j = 0; j < cols.length; ++ j) {
+                let cell = row.cells[cols[j]];
+                let proc = new Cs142TemplateProcessor(cell.innerHTML);
+                cell.innerHTML = proc.fillIn(dict);
+            }
         }
 
         if (table.style.visibility === 'hidden') {
