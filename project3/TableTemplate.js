aca7ad703f497c93c85a08645c738d2d52ae87a8
hlnull <manfeyn@qq.com>
2017-06-05 22:24:53 +0800

    replace header

--- project3/TableTemplate.js
+++ project3/TableTemplate.js
@@ -5,9 +5,9 @@ class TableTemplate {
         var table = document.getElementById(id);
         var rows = table.rows;
         var header = rows.item(0);
-        var proc = new Cs142TemplateProcessor(header);
+        var proc = new Cs142TemplateProcessor(header.innerHTML);
         var newHeader = proc.fillIn(dict);
-        header.replaceWith(newHeader);
+        header.innerHTML = newHeader;
 
         if (columnName === undefined) {
             // handle the table.
