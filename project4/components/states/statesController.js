3911fc6a3fb09e01bb57fb871ad29c14213becc0
hlnull <manfeyn@qq.com>
2017-06-12 09:01:18 +0800

    project 4: able to call filter

--- project4/components/states/statesController.js
+++ project4/components/states/statesController.js
@@ -7,6 +7,17 @@
 cs142App.controller('StatesController', ['$scope', function($scope) {
 
    // Replace this with the code for CS142 Project #4, Problem #2
-   console.log('window.cs142models.statesModel()', window.cs142models.statesModel());
-
-}]);
+   // console.log('window.cs142models.statesModel()', window.cs142models.statesModel());
+   $scope.statesModel = window.cs142models.statesModel();
+   console.log($scope.statesModel);
+}]).filter('myfilter', function() {
+    return function(statesModel, subString) {
+        return statesModel.filter(function (item) {
+            if (subString) {
+                return item.toLowerCase().includes(subString.toLowerCase());
+            } else {
+                return true;
+            }
+        })
+    }
+})
